118c9bfe302aa82fb3fc35a2c112a89921b4f454
Robert Michaud <robert.cn.michaud@hotmail.com>
2020-08-18 18:25:30 -0400

    minor changes

--- lib/cogs/mal.py
+++ lib/cogs/mal.py
@@ -3,21 +3,23 @@ from discord.ext.commands import Cog, command
 from discord import Embed
 from datetime import datetime
 from jikanpy import AioJikan
+from jikanpy.exceptions import APIException
+import json
+
+# import difflib
+# I should parse the titles and find the closest match using diflib.get_close_matches
+
 
 async def jikan(*, query="naruto"):
     async with AioJikan() as aio_jikan:
-        results = await aio_jikan.search(search_type="anime", query=query, parameters={})
+
+        results = await aio_jikan.search("anime", query)
+
+        with open ("anime.json", "w") as f:
+            json.dump(results, f, indent=2)
+
         return results
 
-        # with open ("anime.json", "w") as f:
-        #     json.dump(naruto, f, indent=2)
-
-#         for result in naruto["results"]:
-#             f_result = f"""
-# Title = {result["title"]}
-# Url = {result["url"]}
-# Synopsis = \"{result['synopsis']}\"
-# """
 
 class mal(Cog):
     def __init__(self, bot):
@@ -25,23 +27,29 @@ class mal(Cog):
         
     @command(name="mal")
     async def mal(self, ctx, *, query):
-        result = await jikan(query=query)
-        print(query)
-#         for item in result["results"]:
-#             f_result = f"""
-# Title = {item["title"]}
-# Url = {item["url"]}
-# Synopsis = \"{item['synopsis']}\"
-# """
-#             print(f_result)
 
-        anime = result["results"][0]
+        try:
+            result = await jikan(query=query)
+        except APIException:
+            await ctx.send("Problem connecting to the API. Please try again.")
+            return
+        except Exception:
+            await ctx.send(f"Problem finding {query}. Please try a different query.")
+            
 
-        if end :=anime["end_date"] is None:
+
+        #taking first result for now
+        if len(result["results"]) >= 1:
+            anime = result["results"][0]
+        else:
+            await ctx.send(f"Problem finding {query}. Please try a different query.")
+            return
+
+        if end := anime["end_date"] is None:
             end = "N/A"
         else:
             end = anime["end_date"][:10]
-            
+
         if (episodes := anime["episodes"]) == 0:
             episodes = "N/A"
 
@@ -52,17 +60,11 @@ class mal(Cog):
                    ("**Synopsis:**",f"\"{anime['synopsis']}\"", False),
                    ("**Score:**",anime["score"], True),
                    ("**Episodes:**",episodes, True)]
+                   
         for name, value, inline in fields:
             embed.add_field(name=name, value=value, inline=inline)
             embed.set_image(url=anime["image_url"])
         await ctx.send(embed=embed)
-
-
-
-    
-
-
-
 
 # asyncio.run(jikan())
 def setup(bot):
