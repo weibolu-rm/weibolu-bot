4088738c6aad216f431f11ffdbc038c4a7a52f1d
Robert Michaud <robert.cn.michaud@hotmail.com>
2020-09-17 03:09:14 -0400

    now adding users to db, added welcome & member remove func

--- lib/cogs/welcome.py
+++ lib/cogs/welcome.py
@@ -1,4 +1,7 @@
 from discord.ext.commands import Cog, command
+from ..db import db
+
+WELCOME_CHANNEL = 562190083374055445
 
 class Welcome(Cog):
     def __init__(self, bot):
@@ -6,11 +9,15 @@ class Welcome(Cog):
 
     @Cog.listener()
     async def on_member_join(self, member):
-        pass
+        #TODO: set specific messages and channels in DB
+        db.execute("INSERT INTO exp (UserID) VALUES (?)", member.id)
+        await self.bot.get_channel(WELCOME_CHANNEL).send(f"Welcome {member.mention} to **{member.guild.name}**.")
+        await member.add_roles(member.guild.get_role(756043010004746290))
 
     @Cog.listener()
     async def on_member_remove(self, member):
-        pass   
+        db.execute("DELETE FROM exp WHERE UserID = ?", member.id)   
+        await self.bot.get_channel(WELCOME_CHANNEL).send(f"{member.display_name} has left **{member.guild.name}**.. <:peepoSad:743248118124118056>")
 
 def setup(bot):
     bot.add_cog(Welcome(bot))
