a352158ad192e93da29dffc41332625fd6c44e64
Robert Michaud <robert.cn.michaud@hotmail.com>
2020-12-17 02:13:07 -0500

    changes to .gitignore

---
+++ data/db/build.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS guilds;
+DROP TABLE IF EXISTS members;
+DROP TABLE IF EXISTS member_exp;
+
+
+CREATE TABLE IF NOT EXISTS guilds (
+    guild_id INTEGER PRIMARY KEY,
+    name TEXT,
+    prefix TEXT  DEFAULT "!"
+);
+
+CREATE TABLE IF NOT EXISTS members (
+    member_id INTEGER,
+    guild_id INTEGER,
+    username TEXT,
+    nickname TEXT,
+    discriminator INTEGER,
+    joined_date TEXT,
+    PRIMARY KEY (guild_id, member_id),
+    FOREIGN KEY (guild_id) REFERENCES guilds (guild_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS member_exp (
+    member_id INTEGER,
+    guild_id INTEGER,
+    xp INTEGER DEFAULT 0,
+    level INTEGER DEFAULT 0,
+    xp_lock TEXT DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (guild_id, member_id)
+    FOREIGN KEY (member_id) REFERENCES members (member_id) ON DELETE CASCADE
+);
+
